9fb579e5911f63a9fd80fa2e9b3a4220d3b653b7
Yinchuan Song <songyinchuan@gmail.com>
2012-08-25 22:13:46 -0400

    initial commit

---
+++ config/initializers/secret_token.rb
@@ -0,0 +1,7 @@
+# Be sure to restart your server when you modify this file.
+
+# Your secret key for verifying the integrity of signed cookies.
+# If you change this key, all old signed cookies will become invalid!
+# Make sure the secret is at least 30 characters and all random,
+# no regular words or you'll be exposed to dictionary attacks.
+AsiaSupermarketWebsite::Application.config.secret_token = '33ee412dec0566b6d590f2d54771543d3eba927f9326e17fc9d7bde21673a8078e375b4bde99b8a7464b3e8663babb6663dda4218014ebb085e6576afe09e918'
---
+++ config/initializers/session_store.rb
@@ -0,0 +1,8 @@
+# Be sure to restart your server when you modify this file.
+
+AsiaSupermarketWebsite::Application.config.session_store :cookie_store, key: '_AsiaSupermarketWebsite_session'
+
+# Use the database for sessions instead of the cookie-based default,
+# which shouldn't be used to store highly confidential information
+# (create the session table with "rails generate session_migration")
+# AsiaSupermarketWebsite::Application.config.session_store :active_record_store
